dced633751b8f9b2e46bd4c0ccb0a19d89b4cf24
ATTomatoo <1126160939@qq.com>
2025-04-22 22:38:03 +0800

    更新

--- nonebot_plugin_dorodoro/__init__.py
+++ nonebot_plugin_dorodoro/__init__.py
@@ -17,7 +17,7 @@ __plugin_meta__ = PluginMetadata(
     """,
     extra={
         "author": "ATTomatoo",
-        "version": "1.4.5",
+        "version": "1.4.6",
         "priority": 5,
         "plugin_type": "NORMAL"
     }
